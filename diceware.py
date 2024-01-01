dd279320f2b27cc84d504ea8cf3637238d6bebdd
Camille-Aimé Possamaï <c.pos@orange.fr>
2024-01-01 22:23:56 +0100

    Ajout des boutons Quitter et Sauvegarder

--- diceware.py
+++ diceware.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QSpinBox, QListWidget
+import sys
+
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QSpinBox, QListWidget, QHBoxLayout, QFileDialog
 
 import secrets
 
@@ -7,17 +9,17 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("Diceware")
-        self.setFixedSize(300, 350)
+        self.setFixedSize(300, 388)
 
         main_layout = QVBoxLayout(self)
 
         label1 = QLabel("Nombre de mots de la « Pass Phrase »")
         label2 = QLabel("4 : faible, à éviter")
         label3 = QLabel("5 à 8 : selon le degré de sécurité souhaité")
-        label4 = QLabel("9 et plus : paranoiaque")
+        label4 = QLabel("9 et plus : paranoïaque")
 
-        button = QPushButton("Générer la « Pass Phrase »")
-        button.clicked.connect(self.button_clicked)
+        generate = QPushButton("Générer la « Pass Phrase »")
+        generate.clicked.connect(self.generate_clicked)
         self.nb_mots = QSpinBox()
         self.nb_mots.setRange(4,10)
         self.nb_mots.setValue(5)
@@ -32,10 +34,36 @@ class MainWindow(QWidget):
         main_layout.addWidget(label3)
         main_layout.addWidget(label4)
         main_layout.addWidget(self.nb_mots)
-        main_layout.addWidget(button)
+        main_layout.addWidget(generate)
         main_layout.addWidget(self.rep)
 
-    def button_clicked(self):
+        quit_save = QHBoxLayout()
+        main_layout.addLayout(quit_save)
+
+        quitter = QPushButton("Quitter")
+        quitter.clicked.connect(quit)
+        sauvegarder = QPushButton( "Sauvegarder" )
+        sauvegarder.clicked.connect(self.sauvegarder)
+
+        quit_save.addWidget(sauvegarder)
+        quit_save.addWidget(quitter)
+
+    def sauvegarder(self):
+        dialogue = QFileDialog(self)
+        dialogue.setDirectory("/home/camille")
+        dialogue.setViewMode(QFileDialog.ViewMode.List)
+        
+        if dialogue.exec():
+             file = dialogue.selectedFiles()
+             with open(file[0], 'w') as file:
+                for i in range (0, self.rep.count()):
+                    file.write( self.rep.item(i).text())
+                    file.write(' ')
+                file.write('\n')
+
+            
+
+    def generate_clicked(self):
         self.rep.clear()
         with open("diceware.txt", "r") as file:
             for j in range(0, self.nb_mots.value()):
@@ -45,9 +73,11 @@ class MainWindow(QWidget):
                     file.readline()
                 self.rep.addItem(file.readline().strip())
 
+if __name__ == '__main__':
 
-app = QApplication()
-main_windows = MainWindow()
-main_windows.show()
+    app = QApplication(sys.argv)
+    app.setStyle('Windows')
+    main_windows = MainWindow()
+    main_windows.show()
 
-app.exec()
+    sys.exit(app.exec())
