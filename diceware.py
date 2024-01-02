6931218466faf54ce61ce4f0b2266ebb5898cc9b
Camille-Aimé Possamaï <c.pos@orange.fr>
2024-01-02 18:44:39 +0100

    Ajout de quelques éléments de style (couleurs, polices, bordures, etc.

--- diceware.py
+++ diceware.py
@@ -1,6 +1,8 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QSpinBox, QListWidget, QHBoxLayout, QFileDialog
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
 
 import secrets
 
@@ -9,30 +11,38 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("Diceware")
-        self.setFixedSize(300, 388)
+        self.setFixedSize(275, 425)
 
-        main_layout = QVBoxLayout(self)
+        container = QWidget(self)
+        container.setStyleSheet("background-color: rgb(252, 247, 232)")
 
-        label1 = QLabel("Nombre de mots de la « Pass Phrase »")
-        label2 = QLabel("4 : faible, à éviter")
-        label3 = QLabel("5 à 8 : selon le degré de sécurité souhaité")
-        label4 = QLabel("9 et plus : paranoïaque")
+        main_layout = QVBoxLayout(container)
+
+        label1 = QLabel("Générateur de « Pass Phrases »")
+        label1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        font = QFont("Utopia", 12)
+        label1.setStyleSheet("background-color: rgb(119,137,189); color:white; border: 2px solid rgb(49,68,125); border-radius: 5px; margin-bottom: 10px")
+        label1.setFont(font)
+        label2 = QLabel("Choisir le nombre de mots (entre 4 et 10)\n   4 : faible, à éviter\n   5 à 8 : selon le degré de sécurité souhaité\n   9 et plus : paranoïaque (en 2024 !)")
+        label2.setStyleSheet("background-color: rgb(232,238,252); color: black; border-width: 1px; border-style: outset; border-radius: 2px")
 
         generate = QPushButton("Générer la « Pass Phrase »")
         generate.clicked.connect(self.generate_clicked)
+        generate.setStyleSheet("background-color: rgb(166,155,128)")
         self.nb_mots = QSpinBox()
         self.nb_mots.setRange(4,10)
         self.nb_mots.setValue(5)
+        self.nb_mots.setStyleSheet("background-color: rgb(232,238,252)")
+
  
         self.first_pass = ["sperme", "exil", "éveil", "ulcère", "tract", "rock", "partie", "étang"]
         self.rep = QListWidget()
+        self.rep.setStyleSheet("background-color: rgb(232,238,252)")
         for i in range(0, len(self.first_pass)):
             self.rep.addItem(self.first_pass[i])
 
         main_layout.addWidget(label1)
         main_layout.addWidget(label2)
-        main_layout.addWidget(label3)
-        main_layout.addWidget(label4)
         main_layout.addWidget(self.nb_mots)
         main_layout.addWidget(generate)
         main_layout.addWidget(self.rep)
@@ -41,8 +51,10 @@ class MainWindow(QWidget):
         main_layout.addLayout(quit_save)
 
         quitter = QPushButton("Quitter")
+        quitter.setStyleSheet("background-color: rgb(166,155,128)")
         quitter.clicked.connect(quit)
         sauvegarder = QPushButton( "Sauvegarder" )
+        sauvegarder.setStyleSheet("background-color: rgb(166,155,128)")
         sauvegarder.clicked.connect(self.sauvegarder)
 
         quit_save.addWidget(sauvegarder)
@@ -60,8 +72,6 @@ class MainWindow(QWidget):
                     file.write( self.rep.item(i).text())
                     file.write(' ')
                 file.write('\n')
-
-            
 
     def generate_clicked(self):
         self.rep.clear()
